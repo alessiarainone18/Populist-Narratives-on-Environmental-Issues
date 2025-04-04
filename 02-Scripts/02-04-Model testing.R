154507742206643a46440cff20c5b670b3c0e11f
alessiarainone18 <alessia01@windowslive.com>
2025-04-04 13:58:24 +0200

    Collection of test articles to test the model

---
+++ 02-Scripts/02-04-Model testing.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+setwd("/Users/alessiarainone/Desktop/Data-Mining-Project_Climate-Change-Media-Attention/01-Data")
+
+data <- read.csv("random_sample.csv", sep = ",", header = TRUE)
+
+# Save test data 
+set.seed(123)
+test_sample <- sample_n(filtered_data, 20)
+write.csv(random_sample, "test_sample.csv", row.names = FALSE)
+
+print(test_sample$content)
+
